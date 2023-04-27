20c996c830caa55ef8b26d7e4434e4aef306e228
Anas Hany <96449333+AnasHany219@users.noreply.github.com>
2023-04-27 23:09:37 +0200

    Update Task_3.cpp

--- Task_3/Task_3.cpp
+++ Task_3/Task_3.cpp
@@ -37,8 +37,14 @@ public:
     void set_Waiting_Time(){
         Waiting_Time = Service_Time_Begins - arrival_Time;
     }
+    int get_Waiting_Time(){
+        return Waiting_Time;
+    }
     void set_Time_Customer(){
         Time_Customer = Waiting_Time + Service_Time;
+    }
+    int get_Time_Customer(){
+        return Time_Customer;
     }
     void set_Idle_Time(int IT){
         Idle_Time = IT;
@@ -64,16 +70,19 @@ public:
 int main()
 {
     int Customer, IAT, AT = 0, ST, STB = 0, STE = 0, IT;
+    int Total_IAT = 0, Total_ST = 0, Total_WT = 0, Total_TC = 0, Total_IT = 0;
     cout << "Enter the number of customer: ";
     cin >> Customer;
     vector<Cstmr>cst(Customer);
     for(int i = 0; i < Customer; i++){
         cout << "Enter a Inter-arrival Time & Service Time: ";
         cin >> IAT;
+        Total_IAT += IAT;
         cst[i].set_Inter_arrival_Time(IAT);
         AT += cst[i].get_Inter_arrival_Time();
         cst[i].set_arrival_Time(AT);
         cin >> ST;
+        Total_ST += ST;
         cst[i].set_Service_Time(ST);
         if(STE >= cst[i].get_arrival_Time())
             STB = STE;
@@ -83,17 +92,34 @@ int main()
         STE = STB + ST;
         cst[i].set_Service_Time_Ends(STE);
         cst[i].set_Waiting_Time();
+        Total_WT += cst[i].get_Waiting_Time();
         cst[i].set_Time_Customer();
+        Total_TC += cst[i].get_Time_Customer();
         IT = cst[i].get_Service_Time_Begins() - cst[i-1].get_Service_Time_Ends();
+        Total_IT += IT;
         cst[i].set_Idle_Time(IT);
     }
-
+    /*
+    Average service time
+    Average wait
+    Average time
+    */
     cout << "\nCustomer\tInter-arrival Time\tArrival Time\tService Time\tService Time Begins\tService Time Ends\tWaiting Time\tTime Customer\tIdle Time\n";
     for(int i = 0; i < Customer; i++){
         cout << i+1 << "\t\t";
         cst[i].Print();
 
     }
+    cout << "Total\t\t"
+         << Total_IAT << "\t\t\t\t\t"
+         << Total_ST << "\t\t\t\t\t\t\t\t"
+         << Total_WT << "\t\t"
+         << Total_TC << "\t\t"
+         << Total_IT << '\n'
+         << "\nAverage service time: " << (double)Total_ST/Customer << '\n'
+         << "Average Wait Time: " << (double)Total_WT/Customer << '\n'
+         << "Average Time Customer: " << (double)Total_TC/Customer << '\n';
+
     return 0;
 }
 /*
@@ -112,4 +138,19 @@ Customer        Inter-arrival Time      Arrival Time    Service Time    Service 
 4               6                       8               4               11                      15                      3               7               0
 5               3                       11              1               15                      16                      4               5               0
 6               7                       18              5               18                      23                      0               5               2
+Total           18                                      21                                                              14              35              2
+
+Average service time: 3.5
+Average Wait Time: 2.33333
+Average Time Customer: 5.83333
+*/
+
+/*
+6
+0 4
+1 2
+1 5
+6 4
+3 1
+7 5
 */
